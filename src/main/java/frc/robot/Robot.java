db2332a4cf32ad62568fe15944484367ef0543e0
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2025-01-07 18:39:10 -0500

    copy rest of the code over
    copy rest of the code over

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,101 +1,183 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.CANAssignments;
+import frc.robot.Constants.LoggerConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.Vision;
+import frc.robot.util.ImprovedPowerDistribution;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
-/**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the Main.java file in the project.
- */
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+public class Robot extends LoggedRobot {
+    private Command autonomousCommand;
+    public RobotContainer robotContainer;
+    public ImprovedPowerDistribution robotPD;
 
-  private final RobotContainer m_robotContainer;
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
+        robotPD = new ImprovedPowerDistribution(CANAssignments.PDU_ID, Constants.PDU_TYPE);
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  public Robot() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
-  }
+        Logger.recordMetadata("version", LoggerConstants.RUNNING_UNDER);
+        Logger.recordMetadata("build_commit", BuildConstants.GIT_SHA);
+        Logger.recordMetadata("build_branch", BuildConstants.GIT_BRANCH);
+        Logger.recordMetadata("build_git_uncommitted_changes", String.valueOf(BuildConstants.DIRTY));
+        Logger.recordMetadata("build_timestamp", BuildConstants.BUILD_DATE);
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
+        switch (LoggerConstants.MODE) {
+            case REAL:
+                Logger.addDataReceiver(new WPILOGWriter("/media/sda1/"));
+                if (!LoggerConstants.SILENT_NT4) {
+                    Logger.addDataReceiver(new NT4Publisher());
+                }
+                break;
+            case SIMULATION:
+                Logger.addDataReceiver(new WPILOGWriter(""));
+                Logger.addDataReceiver(new NT4Publisher());
+                break;
+            case REPLAY:
+                setUseTiming(false);
+                String logPath = LogFileUtil.findReplayLog();
+                Logger.setReplaySource(new WPILOGReader(logPath));
+                Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+                break;
+        }
 
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
+        Logger.start();
 
-  @Override
-  public void disabledPeriodic() {}
+        // if you want to stop the robot, use the boolean returned by this method.
+        Constants.CANAssignments.checkAssignments();
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+        // Instantiate our RobotContainer. This will perform all our button bindings,
+        // and put our
+        // autonomous chooser on the dashboard.
+        robotContainer = new RobotContainer();
     }
-  }
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    /**
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like
+     * diagnostics that you want ran during disabled, autonomous, teleoperated and
+     * test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+        // Runs the Scheduler. This is responsible for polling buttons, adding
+        // newly-scheduled
+        // commands, running already-scheduled commands, removing finished or
+        // interrupted commands,
+        // and running subsystem periodic() methods. This must be called from the
+        // robot's periodic
+        // block in order for anything in the Command-based framework to work.
+        CommandScheduler.getInstance().run();
     }
-  }
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
+    /**
+     * This function is called once each time the robot enters Disabled mode.
+     */
+    @Override
+    public void disabledInit() {
+        robotContainer.swerveSubsystem.autonavigator.disable();
+    }
 
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
+    @Override
+    public void disabledPeriodic() {
+        Logger.recordOutput("ll.straightLineDist", Vision.lineOfSightDistance("limelight"));
+        Logger.recordOutput("ll.verticalDist", Vision.verticalPlaneDistance("limelight"));
+        Logger.recordOutput("ll.horizontalDist", Vision.horizontalOffestDistance("limelight"));
+        Logger.recordOutput("ll.3dHypotnuese", Vision.full3DDistance("limelight"));
+    }
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+    /**
+     * This autonomous runs the autonomous command selected by your
+     * {@link RobotContainer} class.
+     */
+    @Override
+    public void autonomousInit() {
+        autonomousCommand = robotContainer.getAutonomousCommand();
 
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
+        // schedule the autonomous command (example)
+        if (autonomousCommand != null) {
+            autonomousCommand.schedule();
+        }
+    }
 
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        // System.out.println(robotContainer.swerveSubsystem.getPose());
+    }
+
+    @Override
+    public void teleopInit() {
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null) {
+            autonomousCommand.cancel();
+        }
+    }
+
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() {
+        if (robotContainer.swerveSubsystem.autonavigator.isEnabled()) {
+            if (MathUtil.applyDeadband(robotContainer.driver.getLeftX(), OIConstants.kDriveDeadband) != 0 || MathUtil.applyDeadband(robotContainer.driver.getLeftY(), OIConstants.kDriveDeadband) != 0 || MathUtil.applyDeadband(robotContainer.driver.getRightX(), OIConstants.kDriveDeadband) != 0) {
+                robotContainer.swerveSubsystem.autonavigator.pauseNavigation();
+            } else {
+                robotContainer.swerveSubsystem.autonavigator.resumeNavigation();
+            }
+        }
+        /* 
+        String llname = LimelightConstants.llObjectDetectionName; 
+        Vision.visionTargetLocation = Vision.transformTargetLocation(robotContainer.swerveSubsystem.getPose(), llname); 
+        HashSet<Integer> tagsFound = new HashSet<>();
+        for (int i = 0; i < LimelightHelpers.getBotPoseEstimate_wpiBlue(LimelightConstants.llLocalizationName).tagCount; i++) {
+            tagsFound.add(LimelightHelpers.getBotPoseEstimate_wpiBlue(LimelightConstants.llLocalizationName).rawFiducials[i].id);
+        }
+        Vision.tagIds.removeIf(tagId -> !tagsFound.contains(tagId));
+        tagsFound.forEach(tagId -> {
+            if(!Vision.tagIds.contains(tagId)){
+                Vision.tagIds.add(tagId);
+            }
+        });
+        //Logger.recordOutput(llname + ".pipelineIndex", LimelightHelpers.getCurrentPipelineIndex(llname));
+        //Logger.recordOutput(llname + ".pipelineName", Vision.getPipelineName(llname));
+        //Logger.recordOutput(llname + ".objectDetected", LimelightHelpers.getTV(llname));
+        */
+    }
+
+    @Override
+    public void testInit() {
+        // Cancels all running commands at the start of test mode.
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,63 +1,97 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+import frc.robot.Constants.LimelightConstants;
+import frc.robot.Constants.Positions;
+import frc.robot.autos.Auto;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+import frc.robot.util.Buttons;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    public final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+    public final ScoringSubsystem scoringSubsystem = new ScoringSubsystem();
+    public final ClimbSubsystem climbSubsystem = new ClimbSubsystem();
+    //public final ArmSubsystem armSubsystem = new ArmSubsystem(); //arm removed =[
+    //public final WristSubsystem wristSubsystem = new WristSubsystem();
+    //public final WristIntakeSubsystem wristIntakeSubsystem = new WristIntakeSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    private final SendableChooser<Command> autoChooser;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  }
+    public XboxController driver = new XboxController(0);
+    public static XboxController gunner = new XboxController(1);
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    public RobotContainer() {
+        SetScoringStateCommand commandShoot = new SetScoringStateCommand(scoringSubsystem, ScoringSubsystem.ScoringState.FIRE, 3);
+        SetScoringStateCommand commandLoad = new SetScoringStateCommand(scoringSubsystem, ScoringSubsystem.ScoringState.LOADING, 2);
+        SetScoringStateCommand commandUnload = new SetScoringStateCommand(scoringSubsystem, ScoringSubsystem.ScoringState.UNLOADING, 2);
+        SetScoringStateCommand autoShoot = new SetScoringStateCommand(scoringSubsystem, ScoringSubsystem.ScoringState.FIRE, ScoringSubsystem.ScoringState.LOADING, 2);
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
+        ParallelCommandGroup autoInit = new ParallelCommandGroup(); // new ParallelCommandGroup((new InstantCommand(() -> wristSubsystem.ampPreset(), wristSubsystem), (new InstantCommand(() -> armSubsystem.underStage(), armSubsystem));
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+        NamedCommands.registerCommand("shoot", autoShoot);
+        NamedCommands.registerCommand("autoInit", autoInit);
+
+        //INIT after registering named commands
+        autoChooser = AutoBuilder.buildAutoChooser("[B]");
+
+        //InstantCommand armLockAmp = new InstantCommand(armSubsystem::ampPreset, armSubsystem);
+        //InstantCommand armLockSource = new InstantCommand(armSubsystem::sourcePreset, armSubsystem);
+        //InstantCommand armLockUnderStage = new InstantCommand(armSubsystem::underStage, armSubsystem);
+        //InstantCommand wristLockAmp = new InstantCommand(wristSubsystem::ampPreset, wristSubsystem);
+        //InstantCommand wristLockSource = new InstantCommand(wristSubsystem::sourcePreset, wristSubsystem);
+        //InstantCommand wristLockUnderStage = new InstantCommand(wristSubsystem::underStage, wristSubsystem);
+
+        //InstantCommand resetArmEncoders = new InstantCommand(armSubsystem::resetArmEncoders, armSubsystem);
+        //ParallelCommandGroup toAmp = new ParallelCommandGroup(armLockAmp, wristLockAmp);
+        //ParallelCommandGroup toSource = new ParallelCommandGroup(armLockSource, wristLockSource);
+        //ParallelCommandGroup toUnderStage = new ParallelCommandGroup(armLockUnderStage, wristLockUnderStage);
+
+        new JoystickButton(gunner, Buttons.RIGHT_BUMPER).onTrue(commandShoot);
+        new JoystickButton(gunner, Buttons.LEFT_BUMPER).onTrue(new Auto(swerveSubsystem, scoringSubsystem, new ArrayList<>(List.of(Positions.FRONTLEFT, Positions.FRONTLEFTMOST, Positions.FRONTRIGHT, Positions.FRONTRIGHTMOST)), 4, Positions.SPEAKER));
+        new JoystickButton(gunner, Buttons.A).onTrue(commandLoad);
+        new JoystickButton(gunner, Buttons.B).onTrue(commandUnload);
+        new JoystickButton(gunner, Buttons.RIGHT_STICK_BUTTON).onTrue(new InstantCommand(scoringSubsystem::increaseRPM));
+        new JoystickButton(gunner, Buttons.LEFT_STICK_BUTTON).onTrue(new InstantCommand(scoringSubsystem::decreaseRPM));
+        //new POVButton(gunner, Buttons.POV_UP).onTrue(toAmp);
+        //new POVButton(gunner, Buttons.POV_LEFT).onTrue(toSource);
+        //new POVButton(gunner, Buttons.POV_DOWN).onTrue(toUnderStage);
+        //new POVButton(gunner, Buttons.POV_RIGHT).onTrue(resetArmEncoders);
+
+        new JoystickButton(driver, Buttons.MENU).onTrue(new InstantCommand(swerveSubsystem::zeroGyro, swerveSubsystem));
+        new JoystickButton(driver, Buttons.VIEW).onTrue(new InstantCommand(swerveSubsystem::switchFR, swerveSubsystem));
+        //new JoystickButton(driver, Buttons.RIGHT_STICK_BUTTON).onTrue(new InstantCommand(swerveSubsystem.autonavigator::toggle, swerveSubsystem));
+        //new JoystickButton(driver, Buttons.X).onTrue(new InstantCommand(() -> swerveSubsystem.autonavigator.navigateTo(Positions.AMP), swerveSubsystem));
+        //new JoystickButton(driver, Buttons.Y).onTrue(new InstantCommand(() -> swerveSubsystem.autonavigator.navigateTo(Positions.SOURCE), swerveSubsystem));
+        //new JoystickButton(driver, Buttons.A).onTrue(new InstantCommand(() -> swerveSubsystem.autonavigator.navigateTo(Positions.SPEAKER), swerveSubsystem));
+        //new JoystickButton(driver, XboxControllerButtons.B).onTrue(new InstantCommand(() -> swerveSubsystem.autonavigator.navigateTo(swerveSubsystem.odometer.getEstimatedPosition().nearest(Constants.FavoritePositions.allPoses)), swerveSubsystem));
+        //new JoystickButton(driver, Buttons.B).onTrue(new InstantCommand(() -> swerveSubsystem.autonavigator.navigateTo(Vision.visionTargetLocation), swerveSubsystem));
+        //new POVButton(driver, Buttons.POV_UP).onTrue(new InstantCommand(() -> new PathPlannerAuto("Starting Point").schedule()));
+        //new POVButton(driver, Buttons.POV_DOWN).onTrue(new InstantCommand(() -> Vision.nextPipeline(LimelightConstants.llObjectDetectionName)));
+
+        SmartDashboard.putData("Auto Chooser", autoChooser);
+
+        swerveSubsystem.setDefaultCommand(new SwerveJoystickDefaultCmd(swerveSubsystem, driver));
+        climbSubsystem.setDefaultCommand(new ClimbCmd(climbSubsystem, gunner));
+        //wristSubsystem.setDefaultCommand(new WristAdjustCmd(wristSubsystem, gunner));
+        //armSubsystem.setDefaultCommand(new ArmAdjustCmd(armSubsystem, gunner));
+        //wristIntakeSubsystem.setDefaultCommand(new WristIntakeCmd(wristIntakeSubsystem, gunner));
+    }
+
+    public Command getAutonomousCommand() {
+        return autoChooser.getSelected();
+    }
 }
