6b224a14ec7bb261c6421a7ed44ebe64b7e2dbea
Comet991 <139285587+Comet991@users.noreply.github.com>
2025-01-11 12:39:22 -0500

    Update BuildConstants.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,7 +5,7 @@ package frc.robot;
  */
 public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
-    public static final String MAVEN_NAME = "FRC_Crescendo";
+    public static final String MAVEN_NAME = "FRC_Reefscape";
     public static final String VERSION = "unspecified";
     public static final int GIT_REVISION = 464;
     public static final String GIT_SHA = "3272977c7d587dab089f9bfcb971727c1bf5b70f";
