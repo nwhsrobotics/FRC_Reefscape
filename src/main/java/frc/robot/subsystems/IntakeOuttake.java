3f0b73bd2908f0fef364876584fb4ff3cb00aa50
RhysesPiecess <123705453+RhysesPiecess@users.noreply.github.com>
2025-01-18 15:28:45 -0500

    feat: we added logic for intake/outake subsystem
    This commit will add the logic to run the servo motor from 0 degrees to 90 degrees. We want to control the servo motor and used the PWM api to program the servo. BREAKING CHANGE: none

--- src/main/java/frc/robot/subsystems/IntakeOuttake.java
+++ src/main/java/frc/robot/subsystems/IntakeOuttake.java
@@ -5,15 +5,29 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.Servo;
 
 public class IntakeOuttake extends SubsystemBase {
+  private PWM servoMoter;
   /** Creates a new IntakeOuttake. */
   public IntakeOuttake() {
-    
+    servoMoter = new PWM(0);
+    servoMoter.setBoundsMicroseconds(1500, 1260, 1250, 1240, 1000);
   }
+
+  public void outtakeOpen() {
+    servoMoter.setPosition(0.5);
+  }
+
+  public void outtakeClose() {
+    servoMoter.setPosition(-0.5);
+  }
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    
   }
 }
