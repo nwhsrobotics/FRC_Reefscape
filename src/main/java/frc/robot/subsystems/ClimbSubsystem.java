3ee8e9d8d14f7588eb3fb1cd11ca30dd9494cfb8
Comet991 <139285587+Comet991@users.noreply.github.com>
2025-01-18 11:51:23 -0500

    added commands for subsystems

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,27 +1,18 @@
-//YOOOOOOO welcome to the climb sub system... the coolest one out there 
-//now DONT FUCKING TOUCH IT 
-
-/*
-_________                        __   ________  ________ 
-\_   ___ \  ____   _____   _____/  |_/   __   \/   __   \
-/    \  \/ /  _ \ /     \_/ __ \   __\____    /\____    /
-\     \___(  <_> )  Y Y  \  ___/|  |    /    /    /    / 
- \______  /\____/|__|_|  /\___  >__|   /____/    /____/  
-        \/             \/     \/                         
-*/
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
-  /** Creates a new ClimbSubsystem. */
+  
+  
+  //Creates a new ClimbSubsystem 
   public ClimbSubsystem() {
 
   }
 
 
 
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
