e6adf1c0cf8676dc631ed47a8627ce06af323aeb
charlie5258 <robinsoncharlie024@gmail.com>
2025-01-22 19:46:30 -0500

    1
    1

--- src/main/java/frc/robot/subsystems/IntakeOuttake.java
+++ src/main/java/frc/robot/subsystems/IntakeOuttake.java
@@ -5,25 +5,26 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.Servo;
 
 public class IntakeOuttake extends SubsystemBase {
-  private PWM servoMoter;
+  private Servo servoMoter;
   /** Creates a new IntakeOuttake. */
   public IntakeOuttake() {
-    servoMoter = new PWM(0);
-    servoMoter.setBoundsMicroseconds(1500, 1260, 1250, 1240, 1000);
+    servoMoter = new Servo(0); // Initialize servo on PWM port 0
+    servoMoter.setBoundsMicroseconds(1500, 1250, 1000);
   }
+  /** Opens the outake mechanism toa present position */
 
   public void outtakeOpen() {
-    servoMoter.setPosition(0.5);
+    servoMoter.setPosition(0.5);  // Mid-point or fully open
   }
+  /** CLoses the outtake mechanism to a present position */
 
   public void outtakeClose() {
-    servoMoter.setPosition(-0.5);
+    servoMoter.setPosition(0.0); // Fully closed
   }
-
+ 
 
   @Override
   public void periodic() {
