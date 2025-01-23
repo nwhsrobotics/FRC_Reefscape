86358a3671eb9dbed1255c27d80af9597121245c
nwhsrobotics <jagbotics4638@gmail.com>
2025-01-22 20:24:46 -0500

    feat: added sysid routine
    this commit will add the sysid routine in the sysid subsystem. record accurate data with sysid. breaking change: adds more complexity for buttons.

--- src/main/java/frc/robot/subsystems/SysId.java
+++ src/main/java/frc/robot/subsystems/SysId.java
@@ -4,14 +4,43 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
-public class SysIdRoutine extends SubsystemBase {
+import com.ctre.phoenix6.SignalLogger;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
+public class SysId extends SubsystemBase {
+  private CANSparkMax Sysmotor;
+  private SysIdRoutine m_sysIdRoutine;
   /** Creates a new SysIdRoutine. */
-  public SysIdRoutine() {}
+  public SysId() {
+    this.Sysmotor = new CANSparkMax(1, MotorType.kBrushless);
+//creates sysid routine in the actual code
+    m_sysIdRoutine = new SysIdRoutine(
+      //set ramp rate voltage and duration
+        new SysIdRoutine.Config(),
+        //lets you record the data and define the subsystem that sysid is in
+        new SysIdRoutine.Mechanism(null, null, null)
+        );
+  }
+
+//running the quasistic test when executed
+public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+  return m_sysIdRoutine.quasistatic(direction);
+}
+//running the dynamic test when executed
+public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+  return m_sysIdRoutine.dynamic(direction);
+}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+
+
 }
