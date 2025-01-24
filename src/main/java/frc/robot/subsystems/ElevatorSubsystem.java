bc4f62f285632a78f3d807e889e289871c0eb68d
Derpnezz <ZygardGX1st@gmail.com>
2025-01-23 19:53:53 -0500

    Feat: Setting up elevator subsystem objects
    Added imports and ImprovedCanSpark has different documentation. BREAKING CHANGE: Bandaid fix performed, null parameter needs to be looked into.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -5,10 +5,23 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.ImprovedCanSpark;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class ElevatorSubsystem extends SubsystemBase {
-  /** Creates a new ElevatorSubsystem. */
-  public ElevatorSubsystem() {}
+  
+  // null parameter is a bandaid fix: FIND OUT WHAT IT DOES
+  private final SparkMax leftElevatorMotor = new ImprovedCanSpark(1, ImprovedCanSpark.MotorKind.NEO550, null, SparkBaseConfig.IdleMode.kBrake);
+  private final SparkMax rightElevatorMotor = new ImprovedCanSpark(2, ImprovedCanSpark.MotorKind.NEO550, null, SparkBaseConfig.IdleMode.kBrake);
+
+
+  public ElevatorSubsystem() {
+
+  }
 
   @Override
   public void periodic() {
