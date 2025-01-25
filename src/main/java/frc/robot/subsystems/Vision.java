a008975999f2dcd5779f5a344a4f68dda3a99e98
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2025-01-25 18:19:36 -0500

    reformat and fix small issues with the equations
    reformat and fix small issues with the equations

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,51 +4,52 @@
 
 package frc.robot.subsystems;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.LimelightHelpers;
+import org.littletonrobotics.junction.Logger;
 
 public class Vision extends SubsystemBase {
-  /** Creates a new Vision. */
-  public Vision() {}
+    /**
+     * Creates a new Vision.
+     */
+    public Vision() {
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run\
-    Logger.recordOutput("ll.straightLineDist", VisionGamePiece.straightLineZDistance("limelight"));
-    Logger.recordOutput("ll.verticalDist", VisionGamePiece.verticalYOffsetDistance("limelight"));
-    Logger.recordOutput("ll.horizontalDist", VisionGamePiece.horizontalOffestXDistance("limelight"));
-    Logger.recordOutput("ll.3dHypotnuese", VisionGamePiece.full3DDistance("limelight"));
-    Logger.recordOutput("ll.2dHypotnuese", VisionGamePiece.hypotenuseDistanceXandZ("limelight"));
-    Logger.recordOutput("ll.detect", LimelightHelpers.getTY(""));
-    //1, 1, Rotation2d.fromDegrees(60)
-    Logger.recordOutput("ll.target", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").toString());
-    Logger.recordOutput("ll.targetX", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getX());
-    Logger.recordOutput("ll.targetY", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getY());
-    Logger.recordOutput("ll.targetDegrees", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getRotation().getDegrees());
-    Logger.recordOutput("ll.targetRadians", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getRotation().getRadians());
-    Logger.recordOutput("ll.targetRotation", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getRotation().getRotations());
-    Logger.recordOutput("ll.getOriginDistance", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getTranslation().getNorm());
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run\
+        Logger.recordOutput("ll.straightLineDist", VisionGamePiece.straightLineZDistance("limelight"));
+        Logger.recordOutput("ll.verticalDist", VisionGamePiece.verticalYOffsetDistance("limelight"));
+        Logger.recordOutput("ll.horizontalDist", VisionGamePiece.horizontalOffestXDistance("limelight"));
+        Logger.recordOutput("ll.3dHypotnuese", VisionGamePiece.full3DDistance("limelight"));
+        Logger.recordOutput("ll.2dHypotnuese", VisionGamePiece.hypotenuseDistanceXandZ("limelight"));
+        Logger.recordOutput("ll.detect", LimelightHelpers.getTY(""));
+        //1, 1, Rotation2d.fromDegrees(60)
+        Logger.recordOutput("ll.target", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").toString());
+        Logger.recordOutput("ll.targetX", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getX());
+        Logger.recordOutput("ll.targetY", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getY());
+        Logger.recordOutput("ll.targetDegrees", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getRotation().getDegrees());
+        Logger.recordOutput("ll.targetRadians", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getRotation().getRadians());
+        Logger.recordOutput("ll.targetRotation", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getRotation().getRotations());
+        Logger.recordOutput("ll.getOriginDistance", VisionGamePiece.transformTargetLocation(new Pose2d(), "limelight").getTranslation().getNorm());
 
 
-
-    Logger.recordOutput("lla.straightLineDist", VisionAprilTag.straightLineZDistance("limelight"));
-    Logger.recordOutput("lla.verticalDist", VisionAprilTag.verticalYOffsetDistance("limelight"));
-    Logger.recordOutput("lla.horizontalDist", VisionAprilTag.horizontalOffsetXDistance("limelight"));
-    Logger.recordOutput("lla.3dHypotnuese", VisionAprilTag.hypotenuseDistanceXandZ("limelight"));
-    Logger.recordOutput("lla.detect", LimelightHelpers.getTY(""));
-    //1, 1, Rotation2d.fromDegrees(60)
-    Logger.recordOutput("lla.target", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").toString());
-    Logger.recordOutput("lla.targetX", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getX());
-    Logger.recordOutput("lla.targetY", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getY());
-    Logger.recordOutput("lla.targetDegrees", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getRotation().getDegrees());
-    Logger.recordOutput("lla.targetRadians", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getRotation().getRadians());
-    Logger.recordOutput("lla.targetRotation", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getRotation().getRotations());
-    Logger.recordOutput("lla.getOriginDistance", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getTranslation().getNorm());
+        Logger.recordOutput("lla.straightLineDist", VisionAprilTag.straightLineZDistance("limelight"));
+        Logger.recordOutput("lla.verticalDist", VisionAprilTag.verticalYOffsetDistance("limelight"));
+        Logger.recordOutput("lla.horizontalDist", VisionAprilTag.horizontalOffsetXDistance("limelight"));
+        Logger.recordOutput("lla.3dHypotnuese", VisionAprilTag.hypotenuseDistanceXandZ("limelight"));
+        Logger.recordOutput("lla.detect", LimelightHelpers.getTY(""));
+        //1, 1, Rotation2d.fromDegrees(60)
+        Logger.recordOutput("lla.target", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").toString());
+        Logger.recordOutput("lla.targetX", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getX());
+        Logger.recordOutput("lla.targetY", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getY());
+        Logger.recordOutput("lla.targetDegrees", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getRotation().getDegrees());
+        Logger.recordOutput("lla.targetRadians", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getRotation().getRadians());
+        Logger.recordOutput("lla.targetRotation", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getRotation().getRotations());
+        Logger.recordOutput("lla.getOriginDistance", VisionAprilTag.transformTargetLocation(new Pose2d(), "limelight").getTranslation().getNorm());
             /* 
         String llname = LimelightConstants.llObjectDetectionName; 
         Vision.visionTargetLocation = Vision.transformTargetLocation(robotContainer.swerveSubsystem.getPose(), llname); 
@@ -66,123 +67,95 @@ public class Vision extends SubsystemBase {
         //Logger.recordOutput(llname + ".pipelineName", Vision.getPipelineName(llname));
         //Logger.recordOutput(llname + ".objectDetected", LimelightHelpers.getTV(llname));
         */
-        
-      }
-    
-      //This method takes in blue alliance april tags and checks if it is the coral station
-      public boolean isBlueAllianceCoralStation(int id) {
-        if (id == 12 || id == 13)  {
-            return true;
-        }
-        return false;
-      }
-      // This method takes in blue alliance april tags and checks if it is the processor
-        public boolean isBlueAllianceProcessor(int id) {
-            if (id == 4) {
-                return true;
-            }
-            return false;
-        }
 
-        //This method takes in blue alliance april tags and checks if it is the reef
-        public boolean isBlueAllianceReef(int id) {
-            if (id == 17 || id == 18 || id == 19 || id == 20 || id == 21 || id == 22) {
-                return true;
-            }
-            return false;
-        }
-        // This method takes in blue alliance april tags and checks if it is the barge
-        public boolean isBlueAllianceBarge(int id) {
-            if (id == 4 || id == 14) {
-                return true;
-            }
-            return false;
-        
-        }
-         
-
-
-// this method takes in a parameter of the april tag and checks if it is at the coral station
-
-public boolean isRedAllianceCoralStation(int id){
-    if(id == 1 || id == 2){
-        return true;
     }
-    return false;
 
-}
-
-// this method takes in a parameter of the april tag and checks if it is at the processor
-
-public boolean isRedAllianceProcessor(int id){
-    if(id == 3){
-        return true;
+    //This method takes in blue alliance april tags and checks if it is the coral station
+    public boolean isBlueAllianceCoralStation(int id) {
+        return id == 12 || id == 13;
     }
-    return false;
 
-}
-
-// this method takes in a parameter of the april tag and checks if it is at the reef
-
-public boolean isRedAllianceReef(int id){
-    if(id == 6 || id == 7 || id == 8 || id == 9 || id == 10 || id == 11){
-        return true;
+    // This method takes in blue alliance april tags and checks if it is the processor
+    public boolean isBlueAllianceProcessor(int id) {
+        return id == 4;
     }
-    return false;
 
-}
-
-// this method takes in a parameter of the april tag and checks if it is at the barge
-
-public boolean isRedAllianceBarge(int id){
-    if(id == 5){
-        return true;
+    //This method takes in blue alliance april tags and checks if it is the reef
+    public boolean isBlueAllianceReef(int id) {
+        return id == 17 || id == 18 || id == 19 || id == 20 || id == 21 || id == 22;
     }
-    return false;
 
-}
+    // This method takes in blue alliance april tags and checks if it is the barge
+    public boolean isBlueAllianceBarge(int id) {
+        return id == 4 || id == 14;
 
-// adjusts the robot position based on april tag position and a preset offset
-public Pose2d transformPosition (Pose2d originalPos,double offsetDistance){
-    
-    double givenX = originalPos.getX();
-    double givenY= originalPos.getY();
-    double givenRot = originalPos.getRotation().getRadians();
-    
-    Pose2d transformedPose = new Pose2d(new Translation2d(givenX, givenY),
-        originalPos.getRotation().plus(originalPos.getRotation()));
-    
-    double adjustedX = givenX * offsetDistance*Math.cos(givenRot);
-    double adjustedY = givenY * offsetDistance*Math.sin(givenRot);
+    }
 
-    Pose2d endResult = new Pose2d(adjustedX,adjustedY,Rotation2d.fromRadians(givenRot));
+    // this method takes in a parameter of the april tag and checks if it is at the coral station
+    public boolean isRedAllianceCoralStation(int id) {
+        return id == 1 || id == 2;
 
-    return endResult;
-    
-}
-public Pose2d scootRight (Pose2d adjustedPos, double scootDist){
-    double initialX = adjustedPos.getX();
-    double initialY= adjustedPos.getY();
-    double initialRot = adjustedPos.getRotation().getRadians();
+    }
 
-    double scootedX = initialX-Math.sin(scootDist);
-    double scootedY = initialY+Math.cos(scootDist);
+    // this method takes in a parameter of the april tag and checks if it is at the processor
+    public boolean isRedAllianceProcessor(int id) {
+        return id == 3;
 
-    Pose2d scootingRight = new Pose2d(scootedX,scootedY,Rotation2d.fromRadians(initialRot));
+    }
 
-    return scootingRight;
-}
+    // this method takes in a parameter of the april tag and checks if it is at the reef
+    public boolean isRedAllianceReef(int id) {
+        return id == 6 || id == 7 || id == 8 || id == 9 || id == 10 || id == 11;
 
-public Pose2d scootLeft (Pose2d adjustedPos, double scootDist){
-    double initialX = adjustedPos.getX();
-    double initialY= adjustedPos.getY();
-    double initialRot = adjustedPos.getRotation().getRadians();
+    }
 
-    double scootedX = initialX+Math.sin(scootDist);
-    double scootedY = initialY-Math.cos(scootDist);
+    // this method takes in a parameter of the april tag and checks if it is at the barge
+    public boolean isRedAllianceBarge(int id) {
+        return id == 5;
 
-    Pose2d scootingRight = new Pose2d(scootedX,scootedY,Rotation2d.fromRadians(initialRot));
+    }
 
-    return scootingRight;
-}
+    //TODO: For all the methods below double check the signs and angle relative to the robot and field
+
+    // adjusts the robot position based on april tag position and a preset offset
+    public Pose2d transformPosition(Pose2d originalPos, double offsetDistance) {
+
+        double givenX = originalPos.getX();
+        double givenY = originalPos.getY();
+        double givenRot = originalPos.getRotation().getRadians();
+
+        double adjustedX = givenX - offsetDistance * Math.cos(givenRot);
+        double adjustedY = givenY - offsetDistance * Math.sin(givenRot);
+
+        Pose2d endResult = new Pose2d(adjustedX, adjustedY, Rotation2d.fromRadians(givenRot));
+
+        return endResult;
+
+    }
+
+    public Pose2d scootRight(Pose2d adjustedPos, double scootDist) {
+        double initialX = adjustedPos.getX();
+        double initialY = adjustedPos.getY();
+        double initialRot = adjustedPos.getRotation().getRadians();
+
+        double scootedX = initialX - scootDist * Math.sin(initialRot);
+        double scootedY = initialY + scootDist * Math.cos(initialRot);
+
+        Pose2d scootingRight = new Pose2d(scootedX, scootedY, Rotation2d.fromRadians(initialRot));
+
+        return scootingRight;
+    }
+
+    public Pose2d scootLeft(Pose2d adjustedPos, double scootDist) {
+        double initialX = adjustedPos.getX();
+        double initialY = adjustedPos.getY();
+        double initialRot = adjustedPos.getRotation().getRadians();
+
+        double scootedX = initialX + scootDist * Math.sin(initialRot);
+        double scootedY = initialY - scootDist * Math.cos(initialRot);
+
+        Pose2d scootingRight = new Pose2d(scootedX, scootedY, Rotation2d.fromRadians(initialRot));
+
+        return scootingRight;
+    }
 }
