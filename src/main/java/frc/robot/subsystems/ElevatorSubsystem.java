df1751cdc5592c8a55aee815882285635dc50435
Derpnezz <ZygardGX1st@gmail.com>
2025-01-27 19:22:28 -0500

    Feat: Set up Relative Encoders
    Plus comments

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.ImprovedCanSpark;
 import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
@@ -17,14 +18,16 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class ElevatorSubsystem extends SubsystemBase {
   
+  // Create ele(vator) motors
   private final SparkMax leftElevatorMotor = new ImprovedCanSpark(25, ImprovedCanSpark.MotorKind.NEO550, null, SparkBaseConfig.IdleMode.kBrake);
   private final SparkMax rightElevatorMotor = new ImprovedCanSpark(26, ImprovedCanSpark.MotorKind.NEO550, null, SparkBaseConfig.IdleMode.kBrake);
 
-  // set up absolute encoders
+  // set up absolute encoders for elevator
   public final CANcoder absoluteEncoder1 = new CANcoder(27);
   public final CANcoder absoluteEncoder2 = new CANcoder(28);
-  // set up relative encoders
-  
+  // set up relative encoders for elevator
+  public RelativeEncoder relativeEncoderLeft = leftElevatorMotor.getEncoder();
+  public RelativeEncoder relativeEncoderRight = rightElevatorMotor.getEncoder();
 
 
   public ElevatorSubsystem() {
