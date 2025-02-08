0ca1f904446f61ea8156101a95f04c981d8c3d0c
ducky786 <rehanks965@gmail.com>
2025-02-07 20:54:24 -0500

    Created lists for locations
    Created lists for locations

--- src/main/java/frc/robot/autos/AutoSelector.java
+++ src/main/java/frc/robot/autos/AutoSelector.java
@@ -1,13 +1,21 @@
 package frc.robot.autos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.Vision;
 
 public class AutoSelector {
-    public AutoSelector (){
+    public AutoSelector (SwerveSubsystem swerve, Vision vision ){
         SendableChooser<Command> autoChooser = new SendableChooser<>();
-        autoChooser.addOption("A to 6A", new Auto(, null, null, null, 0, null))
+        autoChooser.addOption("A to 6A", new Auto(swerve, vision, new ArrayList<String>(List.of("[6A]", "[6B]", "[5A]", "[5B]")), Constants.Positions.CAGE_A));
+        autoChooser.addOption("A to 6A", new Auto(swerve, vision, new ArrayList<String>(List.of("[5A]", "[6B]", "[5A]", "[5B]")), Constants.Positions.CAGE_A));
+        autoChooser.addOption("B to 6A", new Auto(swerve, vision, new ArrayList<String>(List.of("[6A]", "[6B]", "[5A]", "[5B]")), Constants.Positions.CAGE_B));
         SmartDashboard.putData(autoChooser);
     }
 }
