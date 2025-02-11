0d8bd829e8ff5d4392ffaefe97d3c67ec2f687bb
Comet991 <139285587+Comet991@users.noreply.github.com>
2025-02-10 19:48:23 -0500

    FEAT: Commands and CAN IDs
    Added commands and CAN IDs for elevator

---
+++ src/main/java/frc/robot/commands/L2CMD.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+
+public class L2CMD extends Command {
+  
+  private final ElevatorSubsystem elevator;
+  private XboxController gunner;
+
+  public L2CMD(ElevatorSubsystem initElevator , XboxController initGunner ) {
+    this.elevator = initElevator;
+    this.gunner = initGunner; 
+    addRequirements(elevator);
+  }
+
+
+  @Override
+  public void initialize() {}
+
+ 
+  @Override
+  public void execute() {}
+
+ 
+  @Override
+  public void end(boolean interrupted) {}
+
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
