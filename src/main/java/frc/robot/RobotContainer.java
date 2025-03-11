1f59460dbd8fd118948a77a157510fc980018e7f
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2025-03-11 17:39:58 -0400

    fix + last aligning method to add is fully vision based that doesn't need constant tag detection
    need to add the last alignment method

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -167,11 +168,13 @@ public class RobotContainer {
 
         //last alligning resort
         new JoystickButton(driver, Buttons.Y).onTrue(
-            new InstantCommand(() -> swerveSubsystem.drive(0, 0.1, 0, false, true)).repeatedly().until(() -> new WaitCommand(1).isFinished())
+            new RunCommand(() -> swerveSubsystem.drive(0, 0.1, 0, false, true), swerveSubsystem)
+                .withTimeout(1).andThen(new InstantCommand(() -> swerveSubsystem.stopModules(), swerveSubsystem))
         );
 
         new JoystickButton(driver, Buttons.A).onTrue(
-            new InstantCommand(() -> swerveSubsystem.drive(0, -0.1, 0, false, true)).repeatedly().until(() -> new WaitCommand(1).isFinished())
+            new RunCommand(() -> swerveSubsystem.drive(0, 0.1, 0, false, true), swerveSubsystem)
+                .withTimeout(1).andThen(new InstantCommand(() -> swerveSubsystem.stopModules(), swerveSubsystem))
         );
 
         new JoystickButton(driver, Buttons.B).onTrue(
