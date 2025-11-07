773ab1b7741a4b5588b726d86a62433172f1b06d
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2025-11-06 21:05:58 -0500

    Revert "comp day push"
    This reverts commit db57db1e0db47111f14305c97d61b8e76512fb64.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -150,7 +150,7 @@ public class Robot extends LoggedRobot {
 
         LEDSubsystem.setState(LEDSubsystem.LEDState.IDLE);
         robotContainer.intakeoutake.outtakeClose();
-        //robotContainer.algaeArm.Homeposition();
+        robotContainer.algaeArm.Homeposition();
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
